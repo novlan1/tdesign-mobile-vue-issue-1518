86c34ccefc708d198daafc896e00808fd97e28f5
novlan1 <1576271227@qq.com>
2024-11-26 19:23:46 +0800

    fix: 防止报错

--- src/pages/mobile.vue
+++ src/pages/mobile.vue
@@ -1,8 +1,13 @@
 <template>
   moible popup
-  <Popup attach="#teleported" style="padding: 100px" />
+  <Popup v-if="isLoaded" attach="#teleported" style="padding: 100px" />
 </template>
 
 <script lang="ts" setup>
+import { ref, onMounted } from 'vue';
 import { Popup } from 'tdesign-mobile-vue';
+const isLoaded = ref(false);
+onMounted(() => {
+    isLoaded.value = true;
+});
 </script>
--- src/pages/pc.vue
+++ src/pages/pc.vue
@@ -1,8 +1,13 @@
 <template>
   pc popup
-  <Popup attach="#teleported" style="padding: 100px" />
+  <Popup v-if="isLoaded" attach="#teleported" style="padding: 100px" />
 </template>
 
 <script lang="ts" setup>
+import { ref, onMounted } from 'vue';
 import { Popup } from 'tdesign-vue-next';
+const isLoaded = ref(false);
+onMounted(() => {
+    isLoaded.value = true;
+});
 </script>
